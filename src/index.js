5ab6290fefac17eaf65caf5eea26613e7e4a72ce
Aleksey Eroh <alekseyeroh@gmail.com>
2023-02-20 12:45:52 +0300

    feat: resolved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports =
+function check(str, bracketsConfig) {
+  let stack = [];
+  let pairsBrack = Object.fromEntries(bracketsConfig);
+
+  if (str.length % 2 !== 0) return false;
+
+  [...str].forEach((item) => {
+ 
+
+    if (bracketsConfig.flat().includes(item)) {
+      if (stack.length > 0 && pairsBrack[stack[stack.length - 1]] === item) {
+      
+        stack.pop();
+      } else { 
+        stack.push(item);
+      }
+    }
+  });
+  // console.log("stack.length ", stack.length);
+  return stack.length === 0;
 }
+
